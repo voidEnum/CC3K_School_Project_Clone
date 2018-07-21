4b78c3a80bf538ea6b6ea148b4ded97a0b6ecf11
xguanyan <xguanyan@student.cs.uwaterloo.ca>
2018-07-21 19:01:36 -0400

    .o files

---
+++ game.d
@@ -0,0 +1,3 @@
+game.o: game.cc game.h grid.h textdisplay.h cell.h posn.h enemy.h \
+ creature.h entity.h player.h treasure.h item.h treasure_normal.h \
+ invalid_behave.h
---
+++ player.d
@@ -0,0 +1,1 @@
+player.o: player.cc player.h creature.h entity.h posn.h cell.h
