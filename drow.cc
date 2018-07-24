dc3a3c6fbb02a8e73eb9e53f90f6395a0149dc6d
jh22han <jh22han@student.cs.uwaterloo.ca>
2018-07-24 17:15:31 -0400

    drow implement

--- drow.cc
+++ drow.cc
@@ -9,6 +9,9 @@ Drow::Drow(): Player("Drow", 150, 25, 15) {}
 shared_ptr<Player> Drow::useEntity(shared_ptr<Potion_RH> p) {
   modifyHp(p->getPotency() * 3 / 2);
   cout << "my hp is " << getHp() << endl;
+  if (getHp() >= getMaxHp()) {
+    setHp(getMaxHp());
+  }
   return shared_from_this();
 }
 shared_ptr<Player> Drow::useEntity(shared_ptr<Potion_PH> p) {
